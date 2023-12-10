11ec5b03b251c425decdff02172fc97afab2173a
Fatou Toure <tourefo@mail.uc.edu>
2023-12-10 05:22:34 -0500

    Sales tax

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        int priceOfitem = 23;
+        int rate = 0;
+
+        rate = (int) (priceOfitem * 0.05);
+        System.out.println("The total sales tax for a purchase of $" + priceOfitem + " is $" + rate);
     }
 }
+
